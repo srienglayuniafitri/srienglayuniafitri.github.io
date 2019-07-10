092dda381a77c0383d5585dcf689d2b4d3d31fa0
srienglayuniafitri <52686661+srienglayuniafitri@users.noreply.github.com>
2019-07-10 23:14:52 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+/* start  */
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+  /* end  */
 
+  /* start tentang kami */
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+  /* end tentang kami */
